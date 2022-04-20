a41e950f8a2ee7ee16be6735a3caa06231a6856f
Scott Hamrick <2623452+cshamrick@users.noreply.github.com>
2022-04-20 10:05:11 -0500

    chore: initial commit

---
+++ confluence_utils/cli.py
@@ -0,0 +1,39 @@
+import click
+from click_plugins import with_plugins
+import os
+
+
+@click.group()
+@click.version_option(prog_name="confluence-cli")
+def cli():
+    """Commandline interface for Confluence."""
+
+
+@cli.command()
+@click.option(
+    "--url",
+    required=True,
+    help="The URL to the Confluence API. Optionally set with CONFLUENCE_URL.",
+    envvar=["CONFLUENCE_API_URL"],
+)
+@click.option(
+    "--space",
+    required=True,
+    help="Confluence API Space. Optionally set with CONFLUENCE_SPACE.",
+    envvar=["CONFLUENCE_SPACE"],
+)
+@click.option(
+    "--token",
+    required=True,
+    help="Confluence API Token. Optionally set with CONFLUENCE_TOKEN.",
+    envvar=["CONFLUENCE_TOKEN"],
+)
+@click.argument("path", type=click.Path(exists=True, resolve_path=True))
+def publish(path: click.Path, url: str, space: str, token: str):
+    if os.path.isfile(path):
+        click.echo(f"publishing file: {path}")
+    else:
+        click.echo(f"publishing directory: {path}")
+    click.echo(url)
+    click.echo(space)
+    click.echo(token)
