8b86576980b9534964a44c5c3492808cc22950f0
Scott Hamrick <2623452+cshamrick@users.noreply.github.com>
2022-04-21 12:19:20 -0500

    feat(list-pages): add list-pages command

--- confluence_utils/cli.py
+++ confluence_utils/cli.py
@@ -3,9 +3,35 @@ from typing import List, Optional
 
 import click
 from atlassian import Confluence
+from tabulate import tabulate
 
 from .confluence_renderer import convert_to_confluence_content
 from .markdown_parser import parse
+
+
+def client_options(function):  # type: ignore
+    function = click.option(
+        "--url",
+        required=True,
+        help=(
+            "The URL to the Confluence API. Optionally set with"
+            " CONFLUENCE_URL."
+        ),
+        envvar=["CONFLUENCE_API_URL"],
+    )(function)
+    function = click.option(
+        "--space",
+        required=True,
+        help="Confluence Space. Optionally set with CONFLUENCE_SPACE.",
+        envvar=["CONFLUENCE_SPACE"],
+    )(function)
+    function = click.option(
+        "--token",
+        required=True,
+        help="Confluence API Token. Optionally set with CONFLUENCE_TOKEN.",
+        envvar=["CONFLUENCE_TOKEN"],
+    )(function)
+    return function
 
 
 @click.group()
@@ -14,25 +40,33 @@ def cli() -> None:
     """Commandline utils for Confluence."""
 
 
+@cli.command(name="list-pages")
+@client_options
+def list_pages(url: str, space: str, token: str) -> None:
+
+    confluence = Confluence(
+        url=url,
+        token=token,
+    )
+
+    pages = confluence.get_all_pages_from_space(space)
+
+    table = []
+
+    for page in pages:
+        table.append(
+            {
+                "id": page.get("id"),
+                "title": page.get("title"),
+                "url": page.get("_links").get("self"),
+            }
+        )
+
+    click.echo(tabulate(table, headers="keys", tablefmt="fancy_grid"))
+
+
 @cli.command()
-@click.option(
-    "--url",
-    required=True,
-    help="The URL to the Confluence API. Optionally set with CONFLUENCE_URL.",
-    envvar=["CONFLUENCE_API_URL"],
-)
-@click.option(
-    "--space",
-    required=True,
-    help="Confluence API Space. Optionally set with CONFLUENCE_SPACE.",
-    envvar=["CONFLUENCE_SPACE"],
-)
-@click.option(
-    "--token",
-    required=True,
-    help="Confluence API Token. Optionally set with CONFLUENCE_TOKEN.",
-    envvar=["CONFLUENCE_TOKEN"],
-)
+@client_options
 @click.argument("path", type=click.Path(exists=True, resolve_path=True))
 def publish(path: str, url: str, space: str, token: str) -> None:
     confluence = Confluence(
